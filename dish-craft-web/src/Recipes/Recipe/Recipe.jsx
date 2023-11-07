8776b17494821f8fba662841076db59777b26b67
MaryIv76 <ivanova_mary02@mail.ru>
2023-11-07 03:04:37 +0300

    Add transition to recipe page

--- dish-craft-web/src/Recipes/Recipe/Recipe.jsx
+++ dish-craft-web/src/Recipes/Recipe/Recipe.jsx
@@ -1,12 +1,16 @@
 import s from './Recipe.module.css';
+import { NavLink } from 'react-router-dom';
 import RecipeInfo from './RecipeInfo/RecipeInfo';
 
 const Recipe = (props) => {
+
 	return (
-		<div className={s.recipeBlock}>
-			<img src={props.url} alt='Блюдо' />
-			<RecipeInfo title={props.title} readyTime={props.readyTime} complexity={props.complexity} calories={props.calories} description={props.description} key={props.id} />
-		</div>
+		<NavLink className={s.link} to={props.link}>
+			<div className={s.recipeBlock} >
+				<img src={props.url} alt='Блюдо' />
+				<RecipeInfo title={props.title} readyTime={props.readyTime} complexity={props.complexity} calories={props.calories} description={props.description} key={props.id} />
+			</div>
+		</NavLink>
 	);
 }
 
