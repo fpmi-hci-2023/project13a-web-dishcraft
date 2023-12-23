9dff2bdcb115c8ac2e9e35f96bba2d9375fafa85
MaryIv76 <ivanova_mary02@mail.ru>
2023-12-23 09:53:08 +0300

    Edit RecipePage.jsx

--- dish-craft-web/src/Content/RecipePage/RecipePage.jsx
+++ dish-craft-web/src/Content/RecipePage/RecipePage.jsx
@@ -74,9 +74,25 @@ class RecipePage extends React.Component {
 
 			axios.get("https://dish-craft.onrender.com/recipes/" + this.props.currentRecipePage.currentRecipeId + "/comments")
 				.then(response => {
-					console.log(response);
 					let comments = response.data;
-					comments.authorPhotoUrl = '/img/user.jpg';
+
+					comments.map(c => {
+						c.authorPhotoUrl = '/img/user.jpg';
+
+						const date = new Date(c.commentDate);
+						const daysOfWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+						const months = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+
+						const dayOfWeek = daysOfWeek[date.getDay()];
+						const day = date.getDate();
+						const month = months[date.getMonth()];
+						const year = date.getFullYear();
+						const hours = date.getHours();
+						const minutes = date.getMinutes();
+
+						const formattedDate = `${dayOfWeek}, ${day} ${month} ${year} г. ${hours}:${minutes}`;
+						c.commentDate = formattedDate;
+					});
 					this.props.dispatch(setCommentsActionCreator(comments));
 				});
 		}
